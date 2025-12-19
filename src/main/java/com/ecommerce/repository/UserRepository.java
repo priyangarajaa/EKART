7286b7b882cd8bb346df8fe1dd81add0b94ca0f1
Priyanka <priyanka.rmsp876@gmail.com>
2025-12-19 15:43:10 +0530

    User Repository

--- src/main/java/com/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 import com.ecommerce.entity.User;
 public class UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
-    @Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email")
-    Optional<User> findByUsernameOrEmail(@Param("username") String username, @Param("email") String email);
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public Boolean existsByUsername(String username);
+    public Boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?2")
+    public Optional<User> findByUsernameOrEmail(String username, String email);
 }
